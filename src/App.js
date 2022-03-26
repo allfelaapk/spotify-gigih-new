bed8487b70195f584271eec5cdce14b532363918
allfelaapk <alfelaaisyah21@gmail.com>
2022-03-26 22:30:36 +0700

    create a table of tracks

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import logo from "./logo.svg";
 import "./App.css";
 import data from "./data";
 import Track from "./component/Track";
+import dataSpot from "./data/dataSpot";
+import React from "react";
 
 function App() {
   // const clientId = process.env.REACT_APP_CLIENT_ID;
+  const uniq = dataSpot.filter((track, index, arr) => {
+    return arr.map((item) => item.album.id).indexOf(track.album.id) === index;
+  });
+
   return (
     <div className="App">
       {/* <div className="container">
         
         />
       </div> */}
-      <Track
+      {/* <Track
         img={data.album.images[1].url}
         artist={data.album.artists[0].name}
         name={data.album.name}
-      />
+      /> */}
+      <div className="container">
+        {uniq.map((track) => (
+          <React.Fragment key={track.album.id}>
+            <Track
+              img={track.album.images[1].url}
+              artist={track.album.artists[0].name}
+              name={track.album.name}
+            />
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
--- src/component/Track.js
+++ src/component/Track.js
@@ -3,14 +3,20 @@ import "../../src/App.css";
 
 export default function Track({ img, artist, name }) {
   return (
-    <div className="container">
-      <img className="image-opt" src={img} />
-      <h3 className="name">{name}</h3>
-      <p className="artist">{artist}</p>
+    <table className="card">
+      <tbody>
+        <tr>
+          <td>
+            <img className="image-opt" src={img} />
+            <h3 className="name">{name}</h3>
+            <p className="artist">{artist}</p>
 
-      <div>
-        <button className="btn">Select</button>
-      </div>
-    </div>
+            <div>
+              <button className="btn">Select</button>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
